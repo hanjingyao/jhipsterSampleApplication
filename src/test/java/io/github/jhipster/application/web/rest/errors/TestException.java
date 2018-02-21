6ef296dcaf486b6200c4d1b79abc669a3e885db6
DESKTOP-HMA7G92\HJY <1254292279@qq.com>
2018-02-21 23:55:32 +0800

    这是master2的代码

---
+++ src/test/java/io/github/jhipster/application/web/rest/errors/TestException.java
@@ -0,0 +1,9 @@
+package io.github.jhipster.application.web.rest.errors;
+
+import java.io.Serializable;
+
+public class TestException extends Exception implements Serializable{
+    public static void main(String[] args) {
+        System.out.println(22222);
+    }
+}
